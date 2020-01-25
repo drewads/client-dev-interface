8d4d8b2180861b77c543f8072b4255c137126d84
Drew Wadsworth <drew.wadsworth@gmail.com>
2020-01-25 03:05:05 -0800

    Adding initial server functionality only for dev backend

--- client-dev-interface.js
+++ client-dev-interface.js
@@ -1,3 +1,22 @@
 // This is the main node file for client-dev-interface
 // accepts urls of the form /client-dev-interface/resource.js
 // directs http requests to the right resource
+
+const actions = {
+    'create' : require('./create'),
+    'edit' : require('./edit'),
+    'upload' : require('./upload'),
+    'move' : require('./move'),
+    'delete' : require('./delete'),
+    'save' : require('./save-changes')
+};
+
+const url = require('url');
+
+handle = (request, response, systemRoot) => {
+    const query = url.parse(request.url, true);
+    // find part of the url after /client-dev-interface
+    // access the right resource with actions['resource']
+}
+
+exports.handle;
