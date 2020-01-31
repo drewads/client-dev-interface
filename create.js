1d5a0ca0b0dbfbc207a2a713b61409c61607e86d
Drew Wadsworth <drew.wadsworth@gmail.com>
2020-01-31 00:38:14 -0800

    Writing create dev module

--- create.js
+++ create.js
@@ -1,5 +1,13 @@
 const fs = require('fs');
 
+const checkBodyFormat = (body) => {
+    if (body['Directory'] != undefined && body['isDirectory'] != undefined) {
+        if (!body['isDirectory'] && body['Filename'] == undefined) {
+            return false;
+        } else return true;
+    } else return false;
+}
+
 exports.handle = (request, response, systemRoot) => {
     if (request.method === "PUT") {
         let body = [];
@@ -11,32 +19,76 @@ exports.handle = (request, response, systemRoot) => {
         request.on('end', () => {
             body = JSON.parse(Buffer.concat(body).toString());
 
-            // check if body is correct format
+            if (!checkBodyFormat(body)) {
+                response.writeHead(400, 'Request body has incorrect format.');
+                response.end();
+            } else {
+                const dirPathRegex = /\/$/;     // file path ends in /
 
-            const dirPathRegex = /\/$/;     // file path ends in /
+                const location = body['Directory']
+                                + (dirPathRegex.test(body['Directory']) ? '' : '/')
+                                + (body['isDirectory'] ? '' : body['Filename']);
 
-            const location = body['Directory']
-                            + dirPathRegex.test(body['Directory']) ? '' : '/'
-                            + body['isDirectory'] ? '' : body['Filename'];
-
-            // use fs access to see if file already exists
-
-            if (body['Type'] && body['Type'] === 'File') {
-                fs.open(systemRoot + location, (err, fd) => {
-                    // handle error here
-                    // call fs close
-                });
+                // use fs access to see if file already exists
+                fs.access(location, fs.constants.F_OK, (err) => {
+                    if (!err) {
+                        // make page to throw up maybe? prob not
+                        response.writeHead(409, {'Content-Type': 'text/html'});
+                        response.write('File already exists.');
+                        response.end();
+                    } else {
+                        if (!body['isDirectory']) {
+                            fs.open(systemRoot + location, 'a', (err, fd) => {
+                                if (err) {
+                                    // create page to throw?
+                                    response.writeHead(500, {'Content-Type': 'text/html'});
+                                    response.write('Server was unable to open file.');
+                                    response.end();
+                                } else {
+                                    fs.close(fd, (err) => {
+                                        if (err) {
+                                            // create page to throw?
+                                            response.writeHead(500, {'Content-Type': 'text/html'});
+                                            response.write('Server was unable to close file.');
+                                            response.end();
+                                        } else {
+                                            // change this at some point
+                                            response.writeHead(200, {'Content-Type': 'text/html'});
+                                            response.write('Success!');
+                                            response.end();
+                                        }
+                                    });
+                                }
+                            });
+                        } else {
+                            fs.mkdir(systemRoot + location, (err) => {
+                                if (err) {
+                                    // create page to throw?
+                                    response.writeHead(500, {'Content-Type': 'text/html'});
+                                    response.write('Server was unable to create directory.');
+                                    response.end();
+                                } else {
+                                    // change this at some point
+                                    response.writeHead(200, {'Content-Type': 'text/html'});
+                                    response.write('Success!');
+                                    response.end();
+                                }
+                            });
+                        }
+                    }
+                })
+                /*
+                console.log(request.method + ', ' + request.url);
+                console.log('Directory: ' + body['Directory']);
+                console.log('Filename: ' + body['Filename']);
+                console.log('Is Directory: ' + body['isDirectory']);
+                console.log();
+                response.writeHead(200, {'Content-Type': 'text/plain'});
+                response.write('Data received.\n');
+                response.write(request.method + ', ' + request.url + ', ' + JSON.stringify(body));
+                response.end();
+                */
             }
-
-            console.log(request.method + ', ' + request.url);
-            console.log('Directory: ' + body['Directory']);
-            console.log('Filename: ' + body['Filename']);
-            console.log('Type: ' + body['Type']);
-            console.log();
-            response.writeHead(200, {'Content-Type': 'text/plain'});
-            response.write('Data received.\n');
-            response.write(request.method + ', ' + request.url + ', ' + JSON.stringify(body));
-            response.end();
         });
     } else {
         // make error page
