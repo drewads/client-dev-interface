68b23cba8c1afbc7f7db0eb3e84ffbe590055e5c
Drew Wadsworth <drew.wadsworth@gmail.com>
2020-01-30 19:43:55 -0800

    Starting to write create dev function

--- create.js
+++ create.js
@@ -1,4 +1,47 @@
+const fs = require('fs');
+
 exports.handle = (request, response, systemRoot) => {
-    console.log(systemRoot + request.url);
-    response.end();
+    if (request.method === "PUT") {
+        let body = [];
+        
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        });
+
+        request.on('end', () => {
+            body = JSON.parse(Buffer.concat(body).toString());
+
+            // check if body is correct format
+
+            const dirPathRegex = /\/$/;     // file path ends in /
+
+            const location = body['Directory']
+                            + dirPathRegex.test(body['Directory']) ? '' : '/'
+                            + body['isDirectory'] ? '' : body['Filename'];
+
+            // use fs access to see if file already exists
+
+            if (body['Type'] && body['Type'] === 'File') {
+                fs.open(systemRoot + location, (err, fd) => {
+                    // handle error here
+                    // call fs close
+                });
+            }
+
+            console.log(request.method + ', ' + request.url);
+            console.log('Directory: ' + body['Directory']);
+            console.log('Filename: ' + body['Filename']);
+            console.log('Type: ' + body['Type']);
+            console.log();
+            response.writeHead(200, {'Content-Type': 'text/plain'});
+            response.write('Data received.\n');
+            response.write(request.method + ', ' + request.url + ', ' + JSON.stringify(body));
+            response.end();
+        });
+    } else {
+        // make error page
+        response.writeHead(405, {'Content-Type': 'text/html', 'Allow': 'PUT'});
+        response.write('Error 405: Method Not Allowed');
+        response.end();
+    }
 }
