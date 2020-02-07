070f7da3aea222ea0ffc975de3afeeef26fdf5d9
Drew Wadsworth <drew.wadsworth@gmail.com>
2020-02-07 01:16:30 -0800

    Writing asynchronous delete

--- delete.js
+++ delete.js
@@ -21,35 +21,44 @@ const checkBodyFormat = (body) => {
     return body['Filepath'] != undefined && body['isDirectory'] != undefined;
 }
 
-const deleteSystemObject = (request, response, systemRoot) => {
-    // TODO: implement this. info on github issue
+const deleteDirectory = async (filepath) => {
+    try {
+        await fsPromises.rmdir(filepath);
+    } catch (error) {
+        throw new Error('Delete failed: directory could not be removed.'); // fsPromises.rmdir error (see docs)
+    }
 }
 
-const systemObjectNotFound = (response) => {
-    // make page to throw up maybe? prob not
-    response.writeHead(409, {'Content-Type': 'text/html'});
-    // delete failed because..
-    response.write((body['isDirectory'] ? 'Directory' : 'File')
-                    + ' does not exist.');
-    response.end();
+const deleteFile = async (filepath) => {
+    try {
+        await fsPromises.unlink(filepath);
+    } catch (error) {
+        throw new Error('Delete failed: file could not be removed');
+    }
 }
 
-exports.handle = (request, response, systemRoot) => {
+const systemObjectNotFound = () => {
+    throw new Error('Delete failed: system object does not exist.'); // status code 409
+}
+
+exports.handle = async (request, systemRoot) => {
     if (request.method === 'DELETE') {
-        const body = getBody(request);
+        const body = await getBody(request);
 
-        if (!checkBodyFormat(body)) {
-            response.writeHead(400, 'Request body has incorrect format.');
-            response.end();
+        if (checkBodyFormat(body)) {
+            body['Filepath'] = systemRoot + body['Filepath'];
+
+            try {
+                await fsPromises.access(body['Filepath'], fs.constants.F_OK);
+                await body['isDirectory'] ? deleteDirectory(systemRoot + Body['Filepath'])
+                                            : deleteFile(systemRoot + Body['Filepath']);
+            } catch (error) {
+                systemObjectNotFound();
+            }
         } else {
-            fsPromises.access(body['Filepath'], fs.constants.F_OK)
-            .then(() => deleteSystemObject(request, response, systemRoot)) // maybe separate into delete file and dir
-            .catch(error => systemObjectNotFound(response));
+            throw new Error('Delete failed: request body has incorrect format.'); // status code 400, message 'request body has incorrect format'
         }
     } else {
-        // make error page
-        response.writeHead(405, {'Content-Type': 'text/html', 'Allow': 'DELETE'});
-        response.write('Error 405: Method Not Allowed');
-        response.end();
+        throw new Error('Delete failed: method not allowed.'); // status code 405
     }
 }
