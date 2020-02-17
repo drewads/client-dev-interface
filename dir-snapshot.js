5273a9a0adfe0cc19d3668b5bf260c460c9a3039
Drew Wadsworth <drew.wadsworth@gmail.com>
2020-02-16 19:30:47 -0800

    Beginning to write dir-snapshot dev module

--- dir-snapshot.js
+++ dir-snapshot.js
@@ -1,4 +1,29 @@
-exports.handle = (request, response, systemRoot) => {
-    console.log(systemRoot + request.url);
-    response.end();
+/**
+ * This module exports one function, handle, which accepts an
+ * HTTP request and a string indicating what the root of the
+ * server filesystem is, and it takes a snapshot of a directory
+ * on the server's filesystem. The directory is specified
+ * in the HTTP request body.
+ */
+
+'use strict';
+
+const fs = require('fs');
+const fsPromises = fs.promises;
+
+const Success = require('./Success');
+const DevError = require('./DevError');
+const util = require('./util');
+
+exports.handle = async (request, systemRoot) => {
+    // check method is GET
+
+    // get body as JSON
+
+    // check body format
+
+    // take dir snapshot
+    // arrayofdirents = fsPromises.readdir(path, true); <- this returns fs.Dirent objects
+    // arrayofdirents.forEach((dirent) => { arrayToBeReturned.push({'path': dirent.name, 'isDir': dirent.isDirectory()})})
+    // return arrayToBeReturned
 }
