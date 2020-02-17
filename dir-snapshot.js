25e0dfaef227ab848c8384b6624ef49a9ebe2a30
Drew Wadsworth <drew.wadsworth@gmail.com>
2020-02-16 20:44:49 -0800

    Adding request format checks to dir-snapshot dev module

--- dir-snapshot.js
+++ dir-snapshot.js
@@ -15,15 +15,41 @@ const Success = require('./Success');
 const DevError = require('./DevError');
 const util = require('./util');
 
+/**
+ * checkBodyFormat takes a JavaScript object, which is the HTTP request body,
+ * as input and returns a boolean. Returns true if body is formatted correctly
+ * and false otherwise.
+ * 
+ * @param {object} body HTTP request body as a JavaScript object
+ * @return {boolean} true if body is formatted correctly and false otherwise
+ */
+const checkBodyFormat = (body) => {
+    return body['Directory'] != undefined;
+}
+
 exports.handle = async (request, systemRoot) => {
-    // check method is GET
+    // check that HTTP request method is GET
+    if (request.method !== 'GET') {
+        throw new DevError.DevError(DevError.EMET, 405, {}, 'dir-snapshot',
+                                    'method not allowed')
+    }
 
-    // get body as JSON
+    const body = await util.getBodyAsJSON(request) // body is a JavaScript object in the correct case
+    .catch(error => {throw new DevError.DevError(DevError.EBODY, 400, {}, 'dir-snapshot',
+                                                error);});
 
-    // check body format
+    // check that the HTTP request body is formatted correctly
+    if (!checkBodyFormat(body)) {
+        throw new DevError.DevError(DevError.EBODY, 400, {}, 'dir-snapshot',
+                                    'request body has incorrect format');
+    }
 
-    // take dir snapshot
-    // arrayofdirents = fsPromises.readdir(path, true); <- this returns fs.Dirent objects
-    // arrayofdirents.forEach((dirent) => { arrayToBeReturned.push({'path': dirent.name, 'isDir': dirent.isDirectory()})})
-    // return arrayToBeReturned
+    try {
+        // take dir snapshot
+        // arrayofdirents = fsPromises.readdir(path, true); <- this returns fs.Dirent objects
+        // arrayofdirents.forEach((dirent) => { arrayToBeReturned.push({'path': dirent.name, 'isDir': dirent.isDirectory()})})
+        // return arrayToBeReturned
+    } catch (error) {
+
+    }
 }
