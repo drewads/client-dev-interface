fde664515e6ceb425588ef405163c85d10e665c0
Drew Wadsworth <drew.wadsworth@gmail.com>
2020-02-27 17:12:07 -0800

    learning and planning for upload dev module

--- upload.js
+++ upload.js
@@ -1,4 +1,27 @@
+/**
+* This module exports one function, handle, which accepts an
+* HTTP request and a string indicating what the root of the
+* server filesystem is, and it saves an uploaded file on the
+* server's filesystem.
+*/
+
+'use strict';
+
+const fs = require('fs');
+const fsPromises = fs.promises;
+
+const Success = require('./Success');
+const DevError = require('./DevError');
+const util = require('./util');
+const formidable = require('formidable');
+
+/**
+ * 
+ */
 exports.handle = (request, response, systemRoot) => {
-    console.log(systemRoot + request.url);
-    response.end();
+    // check method is PUT
+    // use formidable to parse
+    // handle errors
+    // check that directory path falls in filesystem
+    // see if maybe we can check stuff before saving files to system
 }
